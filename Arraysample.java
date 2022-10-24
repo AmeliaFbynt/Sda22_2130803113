b07347eb6eb625815753d22933dddd9f6e7d5897
AmeliaFbynt <113447615+AmeliaFbynt@users.noreply.github.com>
2022-10-24 14:43:27 +0700

    Add files via upload

---
+++ Arraysample.java
@@ -0,0 +1,15 @@
+public class arraysample {
+    public static void main (String[]args){
+        int[]ages=new int [10];
+        for (int i=0;i<ages.length;i++){
+            System.out.print(ages[i]);
+
+            
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "arraysample []";
+    }
+}
---
+++ insertionsort.java
@@ -0,0 +1,23 @@
+public class insertionsort {
+    /**
+     * @param arr
+     */
+    void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i){
+            final int key = arr [i];
+            int j = i - 1;
+            /* Move elements of arr [0..i-1], that are 
+            greater than key, to one position ahead
+            of their current position*/
+            while (j >= 0 && arr [j] > key){
+                arr[j + 1] = arr [j];
+                j=j-1;
+
+            }
+            arr[j+1] = key;
+
+        }
+    }
+}
